e9c7128b8e9a2f4be047e6c304096aab6c2e5e6b
Lamthong Keophalychanh <thea913@gmail.com>
2015-11-12 20:25:13 -0600

    project1 logic

--- project1/script/app.js
+++ project1/script/app.js
@@ -1,39 +1,72 @@
-//I want to make 2 variables.
-var playerMove = prompt('Choose rock, paper, or scissors');
-var compMove = Math.random();
+// I want to make 2 variables, players.
+//global variables that can be used anywhere because they are outside of the function.
+var playerMove;
+var compMove;
+
+
+// then call the function
+// but lets make sure there is an answer
+function myFunction() {
+  playerMove = document.getElementById('playerMove').value;
+  //takes whatever text in the input box and assigns it to playerMove but only when the submit button is clicked
+  //if no text is in the input box but submit is clicked then...
+  //alert('Choose rock, paper, or scissors');
+  compMove = Math.random();
+
+  if (playerMove.length != 0) {
+    computerMove();
+    compare(playerMove, compMove);
+  } else {
+    alert('Choose rock, paper, or scissors');
+  }
+}
+
+
+// Generate a computer move
+function computerMove() {
+  var comp = Math.random();
+//give the computer some parameters
+
+  if (comp < 0.34) {
+   compMove = "rock";
+  } else if(comp <= 0.67) {
+   compMove = "paper";
+  } else {
+   compMove = "scissors";
+  }
+
+}
 
 
 //I want to make a function to compare player and comp's moves.
-function myFunction() {
 
-   function compare(playerMove, compMove) {
-     document.getElementById('compare').innerHTML = 'result';
-     if (playerMove === compMove) {
-       document.getElementById('result').innerHTML = "It\s a tie!";
-     }
-   }
 
-   if (playerMove === 'rock') {
-     if (compMove === "scissors") {
-       document.getElementById('result').innerHTML = "It\s The Rock!";
-     } else {
-       document.getElementById('result').innerHTML = "It\s a wrap for you!";
-     }
-   }
+  function compare(playerMove, compMove) {
+    console.log(compMove);
 
-   else if (playerMove === 'paper') {
-     if (compMove === 'rock') {
-       document.getElementById('result').innerHTML = "It\s a wrap for you!";
-     } else {
-       document.getElementById('result').innerHTML = "You just got cut!";
-     }
-   }
+    if (playerMove === compMove) {
+      document.getElementById('result').innerHTML = "It\'s a tie!";
+    } else if (playerMove === 'rock') {
+      if (compMove === "scissors") {
+        document.getElementById('result').innerHTML = "It\'s The Rock!";
+      } else {
+        document.getElementById('result').innerHTML = "It\'s a wrap for you!";
+      }
+    } else if (playerMove === 'paper') {
+      if (compMove === 'rock') {
+        document.getElementById('result').innerHTML = "It\'s a wrap for you!";
+      } else {
+        document.getElementById('result').innerHTML = "You just got cut!";
+      }
+    } else if (playerMove === 'scissors') {
+      if (compMove === "rock") {
+        document.getElementById('result').innerHTML = "It\'s The Rock!";
+      } else {
+        document.getElementById('result').innerHTML = "You just got cut!";
+      }
+    }
 
-   else if (playerMove === 'scissors') {
-     if (compMove === "rock") {
-       document.getElementById('result').innerHTML = "It\s The Rock!";
-     } else {
-       document.getElementById('result').innerHTML = "You just got cut!";
   }
-}
-}
+
+
+//}
